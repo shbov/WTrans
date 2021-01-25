e94f4ecb38a6aa08d9c2954322d15adf71a9ef7c
Andrew Shubnikov <a.shbov@yandex.ru>
2021-01-25 03:14:28 +0300

    add admin user

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'shbov',
+            'email' => 'a.shbov@yandex.ru',
+            'password' => Hash::make('shbov'),
+        ]);
+    }
+}
