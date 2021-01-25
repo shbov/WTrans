5720f4bcbae653b44003cdf9a802950694a08ff4
Andrew Shubnikov <a.shbov@yandex.ru>
2021-01-25 03:51:26 +0300

    Add relation with languages

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    public function language()
+    {
+        return $this->belongsTo('App\Models\Language', 'language_id');
+    }
+
+    public function originalLanguage()
+    {
+        return $this->belongsTo('App\Models\Language', 'original_language_id');
+    }
 }
